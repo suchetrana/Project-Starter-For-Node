d53061c2c95953ce91d51e2b5d642f5dc3149929
suchetrana <suchetrana27@gmail.com>
2025-11-02 22:03:59 +0530

    Added git ignore file

--- src/config/index.js
+++ src/config/index.js
@@ -1,6 +1,7 @@
 const dotenv = require('dotenv');
-
 dotenv.config();
+
+console.log("Loaded PORT from env:", process.env.PORT); 
 
 module.exports = {
     PORT: process.env.PORT
--- src/server.js
+++ src/server.js
@@ -1,8 +1,8 @@
 const express = require('express');
-const { PORT } = require('./config')
+const { PORT } = require('./config/index')
 
 const app = express();
 
 app.listen(PORT, () => {
     console.log(`Server running http://localhost:${PORT}`);
-})
+});
